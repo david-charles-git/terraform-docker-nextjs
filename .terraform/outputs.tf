e71255117cfbee38bcec28edf74f0db8a742859a
David <david.charles@addmustard.com>
2023-06-01 15:48:58 +0100

    test commit

--- .terraform/outputs.tf
+++ .terraform/outputs.tf
@@ -2,14 +2,14 @@ output "aws_ecr_respoitory_url" {
   value = module.ecr.ecr_repository_url
 }
 
-output "aws_ecs_alb_domain_name" {
-  value = module.ecs.aws_alb_dns_name
-}
+# output "aws_ecs_alb_domain_name" {
+#   value = module.ecs.aws_alb_dns_name
+# }
 
-output "aws_domain_root" {
-  value = local.aws_route53_record_name_root
-}
+# output "aws_domain_root" {
+#   value = local.aws_route53_record_name_root
+# }
 
-output "aws_domain_www" {
-  value = local.aws_route53_record_name_www
-}
+# output "aws_domain_www" {
+#   value = local.aws_route53_record_name_www
+# }
